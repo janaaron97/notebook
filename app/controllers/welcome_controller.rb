b5021b9ed44fddfc6dbe48e1e50e02047ca61bb8
Aaron Janke <aaronljanke@gmail.com>
2017-01-13 08:31:28 -0600

    cleaned up controller for welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-  	if user_signed_in? && Doc.count != 0
-    	@doc = current_user.docs.last
-    end
+    @doc = current_user.docs.last
   end
 end
